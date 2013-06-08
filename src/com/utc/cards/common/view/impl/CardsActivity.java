1561c07997310028b4e91ed2187754bb4de666b6
Benoit Soufflet <benoit.soufflet@gmail.com>
2013-06-08 17:58:26 +0200

    Ajout d'un "slidermenu" sur l'écran d'accueil pour entrer l'adresse IP de l'hôte de la partie (main container)

--- src/com/utc/cards/common/view/impl/CardsActivity.java
+++ src/com/utc/cards/common/view/impl/CardsActivity.java
@@ -1,24 +1,25 @@
 package com.utc.cards.common.view.impl;
 
-import static com.utc.cards.Constants.GMAIL;
 import static com.utc.cards.Constants.JADE_CARDS_PREFS_FILE;
+import static com.utc.cards.Constants.LOCAL_IP;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.Gravity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.utc.cards.R;
@@ -34,112 +35,132 @@ public class CardsActivity extends Activity
     public final static String EXTRA_MESSAGE = "com.utc.cards.EXTRA_MESSAGE";
     public static final int PLAYER_REQUEST = 0;
     public static final int HOST_REQUEST = 1;
-    private Point _screenDimention = new Point();
+
+    private DrawerLayout mDrawerLayout;
+    private LinearLayout mDrawerPanel;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     // private MyReceiver myReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_cards);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
 
-	getScreenSize();
-
-	setLogoDimension();
+        setSlidingMenu(savedInstanceState);
     }
 
-    private void setLogoDimension()
+    private void setSlidingMenu(Bundle savedInstanceState)
     {
-	ImageView img = (ImageView) findViewById(R.id.logoCards);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerPanel = (LinearLayout) findViewById(R.id.left_drawer);
 
-	double diff = 0.5;
-	double w = _screenDimention.y * 0.7;
-	double h = w * diff;
+        // set a custom shadow that overlays the main content when the drawer opens
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
 
-	LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams((int) w,
-		(int) h);
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
 
-	lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.TOP;
-	lp.topMargin = (int) (_screenDimention.y * 0.1);
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+        mDrawerLayout, /* DrawerLayout object */
+        R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
+        R.string.drawer_open, /* "open drawer" description for accessibility */
+        R.string.drawer_close /* "close drawer" description for accessibility */
+        )
+        {
+            public void onDrawerClosed(View view)
+            {
+                getActionBar().setTitle("CARDS");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
 
-	img.setLayoutParams(lp);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
-	setUserGmail();
-	// myReceiver = new MyReceiver();
+                EditText myEditText = (EditText) findViewById(R.id.hostIpAddressEditText);
+                imm.hideSoftInputFromWindow(myEditText.getWindowToken(), 0);
+            }
 
-	// IntentFilter refreshChatFilter = new IntentFilter();
-	// refreshChatFilter.addAction("jade.demo.chat.REFRESH_CHAT");
-	// registerReceiver(myReceiver, refreshChatFilter);
+            public void onDrawerOpened(View drawerView)
+            {
+                getActionBar().setTitle("Options");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        setIpAddress();
+
+        setContentFrame();
+    }
+
+    private void setIpAddress()
+    {
+        SharedPreferences settings = getSharedPreferences(JADE_CARDS_PREFS_FILE, Context.MODE_PRIVATE);
+        String localHost = settings.getString(LOCAL_IP, "");
+        // Ce n'est pas la bonne adresse, mais comme il faut ��tre sur le m��me
+        // r��seau on peut afficher la m��me adresse ip pour la corriger plus
+        // rapidement
+        String localIpAddress = localHost;
+
+        final EditText hostIpAddressEditText = (EditText) findViewById(R.id.hostIpAddressEditText);
+        hostIpAddressEditText.setText(localIpAddress);
+    }
+
+    private void setContentFrame()
+    {
+        // update the main content by replacing fragments
+        Fragment fragment = new FragmentCardsActivity();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(R.menu.cards, menu);
-	return true;
-    }
-
-    private void getScreenSize()
-    {
-	Display display = getWindowManager().getDefaultDisplay();
-	display.getSize(_screenDimention);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.cards, menu);
+        return true;
     }
 
     public void playerMode(View view)
     {
-	Intent intent = new Intent(this, PlayerMenuActivity.class);
+        Intent intent = new Intent(this, PlayerMenuActivity.class);
 
-	startActivityForResult(intent, PLAYER_REQUEST);
+        startActivityForResult(intent, PLAYER_REQUEST);
     }
 
     public void hostMode(View view)
     {
-	Intent intent = new Intent(this, TableSelectGameActivity.class);
-	startActivityForResult(intent, HOST_REQUEST);
+        Intent intent = new Intent(this, TableSelectGameActivity.class);
+        startActivityForResult(intent, HOST_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
 
-	if (requestCode == PLAYER_REQUEST)
-	{
-	    if (resultCode == RESULT_CANCELED)
-	    {
-		// The player activity was closed.
-		log.info("onActivityResult() : Stopping Jade...");
-		PlayerAgentManager.instance().stopAgentContainer();
-	    }
-	} else if (requestCode == HOST_REQUEST)
-	{
-	    if (resultCode == RESULT_CANCELED)
-	    {
-		// The host activity was closed.
-		// _log.info("onActivityResult() : Stopping Jade...");
-		// PlayerAgentManager.instance().stopAgentContainer();
-	    }
-	}
-    }
-
-    private void setUserGmail()
-    {
-	AccountManager manager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
-	Account[] list = manager.getAccounts();
-	for (Account account : list)
-	{
-	    if (account.type.equalsIgnoreCase("com.google"))
-	    {
-		SharedPreferences settings = getSharedPreferences(
-			JADE_CARDS_PREFS_FILE, Context.MODE_PRIVATE);
-		SharedPreferences.Editor editor = settings.edit();
-		editor.putString(GMAIL, account.name);
-		editor.commit();
-		log.info("JADE_CARDS_PREFS_FILE :");
-		log.info(GMAIL + " = " + account.name);
-	    }
-	}
+        if (requestCode == PLAYER_REQUEST)
+        {
+            if (resultCode == RESULT_CANCELED)
+            {
+                // The player activity was closed.
+                log.info("onActivityResult() : Stopping Jade...");
+                PlayerAgentManager.instance().stopAgentContainer();
+            }
+        }
+        else if (requestCode == HOST_REQUEST)
+        {
+            if (resultCode == RESULT_CANCELED)
+            {
+                // The host activity was closed.
+                // _log.info("onActivityResult() : Stopping Jade...");
+                // PlayerAgentManager.instance().stopAgentContainer();
+            }
+        }
     }
 
     // private class MyReceiver extends BroadcastReceiver
